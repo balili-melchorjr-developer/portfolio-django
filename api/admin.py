b99dc0d0ae677f44be42c7866ec1f3385715b13f
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-02-27 09:22:23 +0800

    Configured admin.py with models and also added the api in settings.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    City,
+    Province,
+    Country,
+    Company,
+    Position,
+    Experience,
+    Technology,
+    Category,
+    Project
+)
+
+admin.site.register(City)
+admin.site.register(Province)
+admin.site.register(Country)
+admin.site.register(Company)
+admin.site.register(Position)
+admin.site.register(Experience)
+admin.site.register(Technology)
+admin.site.register(Category)
+admin.site.register(Project)
--- api/models.py
+++ api/models.py
@@ -3,33 +3,42 @@ from django.contrib.auth.models import User
 
 class City(models.Model):
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='city_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='city_modified_by')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Cities'
 
 class Province(models.Model):
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='province_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='province_modified_by')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Provinces'
 
 class Country(models.Model):
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='country_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='country_modified_by')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Countries'
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
@@ -39,25 +48,26 @@ class Company(models.Model):
     province = models.ForeignKey(Province, on_delete=models.CASCADE)
     zip_code = models.CharField(max_length=200)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='company_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='company_modified_by')
 
     def __str__(self):
         return self.name
-
+    
+    class Meta:
+        verbose_name_plural = 'Companies'
 
 class Position(models.Model):
-    name = models.Charfield(max_length=200)
-    created_at = models.DateTimeField(auto_add_now=True)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='position_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='position_modified_by')
 
     def __str__(self):
         return self.name
-
 
 class Experience(models.Model):
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
@@ -65,9 +75,9 @@ class Experience(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.TextField()
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='experience_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='experience_modified_by')
 
     def __str__(self):
@@ -77,33 +87,39 @@ class Technology(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField()
-    created_at = models.DateTimeField(auto_add_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='techonology_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='techonology_modified_by')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Technologies'
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    technology = models.ManyToManyField(Technology, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_add_now=True)
+    technology = models.ManyToManyField(Technology)
+    created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='category_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='category_modified_by')
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    technology = models.ManyToManyField(Technology, on_delete=models.CASCADE)
-    reated_at = models.DateTimeField(auto_add_now=True)
+    technology = models.ManyToManyField(Technology)
+    reated_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_created_by')
-    modified_at = models.DateTimeField(auto_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     modified_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_modified_by')
 
     def __str__(self):
